4e3a6426e5f8cf34e196344c385d917aa4b52d08
Bryanorionn <bryanorionn@hotmail.com>
2025-10-23 18:58:42 +0530

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,24 +1,15 @@
-import discord
-from discord.ext import commands
-import os
-import webserver
+from flask import Flask
+from threading import Thread
 
-DISCORD_TOKEN = os.environ['discordkey']
-MENTIONED_USER_ID = '1220762300068266045'
+app = Flask('')
+@app.route ('/')
+def home () :
+    return "Discord bot ok"
 
-intents = discord. Intents.default()
-intents.message_content = True
-bot = commands. Bot(command_prefix='!', intents=intents)
+def run():
+    app.run(host="0.0.0.0", port=8080)
 
-@bot.event
-async def on_message(message):
-    if MENTIONED_USER_ID in [str(user.id) for user in message.mentions]:
-        try:
-            for i in range(50):
-                await message.author.send("GET SPAMMED")
-        except discord.Forbidden:
-            await message.channel.send(f"{message.author.mention}, I tried to DM you but couldn't. Please enable DMs. ")
-        await bot.process_commands(message)
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
-webserver.keep_alive()
-bot.run(DISCORD_TOKEN)
